7bfce28f22ed4d256e91b5ad73e7656c7aa1a982
Hknots <Hknots99@gmail.com>
2023-11-08 09:06:37 +0100

    remove useless variable

--- src/components/datatable/DataTableRow.jsx
+++ src/components/datatable/DataTableRow.jsx
@@ -6,10 +6,9 @@ function DataTableRow({ row, columns }) {
     return (
         <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
             {columns.map((column) => {
-                const value = row[column.id];
                 return (
                     <TableCell key={column.id} align={column.align}>
-                        {column.format ? column.format(row) : value}
+                        {column.format ? column.format(row) : row[column.id]}
                     </TableCell>
                 );
             })}
