86539e56595f7dac17581aaf0e813132311aa234
Hknots <hknots99@gmail.com>
2024-09-05 17:05:24 +0200

    add constant for menu items

---
+++ app/constants/menu.ts
@@ -0,0 +1,23 @@
+type MenuLink = {
+  name: string,
+  href: string
+};
+
+export const MENU_LINKS: MenuLink[] = [
+  {
+    "name": "Kontrakter",
+    "href": "/kontrakter",
+  },
+  {
+    "name": "Hendelser",
+    "href": "/hendelser",
+  },
+  {
+    "name": "Entiteter",
+    "href": "/entiteter",
+  },
+  {
+    "name": "Konsumere",
+    "href": "/konsumere",
+  },
+]
