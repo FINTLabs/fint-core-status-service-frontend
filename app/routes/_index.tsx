04f11057bb4d23e2bdbb9c19c57b8d7c8490f872
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-10-16 15:49:35 +0200

    add header to hendelser

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import {MENU_LINKS} from "~/constants/menu";
 
 export default function Index() {
   return (
-    <Box>
+    <Box style={{ marginTop: "5px"}} >
       <VStack gap="6" justify={'center'} align="center">
         <HGrid className="pt-4" gap="3" columns={{xs: 1, sm: 2, md: 4, lg: 4, xl: 4}}>
           {MENU_LINKS.map((menuLink, index) => (
--- app/routes/hendelser._index.tsx
+++ app/routes/hendelser._index.tsx
@@ -6,6 +6,7 @@ import {json} from "@remix-run/node";
 import {HendelserApi} from "~/api/HendelserApi";
 import {FintEvent} from "~/components/hendelser/event/FintEvent";
 import {useLoaderData} from "@remix-run/react";
+import Index from "~/routes/_index";
 
 export const loader = async () => {
     const events = await HendelserApi.getHendelser()
@@ -59,49 +60,58 @@ export default function FintEventTable() {
     };
 
     return (
-        <div style={{marginTop: "30px", marginRight: '30px', marginLeft: '30px'}}>
-            <HStack justify="center">
-                {searchBar()}
-            </HStack>
-            <Table zebraStripes>
-                <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell scope="col">Corrid</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">OrgId</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Har Feil</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Har response</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Resurs</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Request time</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {events.map((event, i) => (
-                    <React.Fragment key={i}>
-                        <Table.Row onClick={() => toggleRow(i)} style={{ cursor: 'pointer' }}>
-                            <Table.DataCell>{event.corrId}</Table.DataCell>
-                            <Table.DataCell>{event.ordId}</Table.DataCell>
-                            <Table.DataCell>{event.hasError ? "Ja" : "Nei"}</Table.DataCell>
-                            <Table.DataCell>{event.responseEvent ? "Ja" : "Nei"}</Table.DataCell>
-                            <Table.DataCell>{event.requestEvent.resourceName}</Table.DataCell>
-                            <Table.DataCell>{formatTime(event)}</Table.DataCell>
+        <div>
+            <div>
+                <Index />
+            </div>
+            <div style={{ marginTop: "30px", marginRight: "30px", marginLeft: "30px" }}>
+                <HStack justify="center">{searchBar()}</HStack>
+                <Table zebraStripes>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell scope="col">Corrid</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">OrgId</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Har Feil</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Har response</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Resurs</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Request time</Table.HeaderCell>
                         </Table.Row>
-                        {expandedRows.has(i) && (
-                            <Table.Row>
-                                <Table.DataCell colSpan={4} style={{ paddingLeft: '40px' }}>
-                                    {event.responseEvent ? (
-                                        <div>
-                                            <strong>Response Event:</strong> {event.responseEvent.corrId}
-                                        </div>
-                                    ) : (
-                                        <div>No Response Event</div>
-                                    )}
-                                </Table.DataCell>
-                            </Table.Row>
-                        )}
-                    </React.Fragment>
-                ))}
-            </Table.Body>
-        </Table>
+                    </Table.Header>
+                    <Table.Body>
+                        {events.map((event, i) => (
+                            <React.Fragment key={i}>
+                                <Table.Row
+                                    onClick={() => toggleRow(i)}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    <Table.DataCell>{event.corrId}</Table.DataCell>
+                                    <Table.DataCell>{event.ordId}</Table.DataCell>
+                                    <Table.DataCell>{event.hasError ? "Ja" : "Nei"}</Table.DataCell>
+                                    <Table.DataCell>
+                                        {event.responseEvent ? "Ja" : "Nei"}
+                                    </Table.DataCell>
+                                    <Table.DataCell>{event.requestEvent.resourceName}</Table.DataCell>
+                                    <Table.DataCell>{formatTime(event)}</Table.DataCell>
+                                </Table.Row>
+                                {expandedRows.has(i) && (
+                                    <Table.Row>
+                                        <Table.DataCell colSpan={4} style={{ paddingLeft: "40px" }}>
+                                            {event.responseEvent ? (
+                                                <div>
+                                                    <strong>Response Event:</strong>{" "}
+                                                    {event.responseEvent.corrId}
+                                                </div>
+                                            ) : (
+                                                <div>No Response Event</div>
+                                            )}
+                                        </Table.DataCell>
+                                    </Table.Row>
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </Table.Body>
+                </Table>
+            </div>
         </div>
-    )
+    );
 }
