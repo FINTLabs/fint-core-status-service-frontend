775121e98b5f2e45628c98a6bbc6ef79c2a96b4a
Hknots <hknots99@gmail.com>
2024-12-10 12:30:46 +0100

    adjust StatusApi routes and methods

--- app/api/StatusApi.ts
+++ app/api/StatusApi.ts
@@ -1,36 +1,48 @@
 import * as process from "process";
 import {FintEvent} from "~/types/Event";
 import {AdapterContract} from "~/types/AdapterContract";
+import {backendRoutesMap} from "~/api/backendRoutes";
+import {HeaderProperties} from "~/components/root/HeaderProperties";
 
-const API_URL = `${process.env.PUBLIC_API_URL}`;
+const PROFILE = process.env.PROFILE
 
 export class StatusApi {
   static async getHendelser(env: string): Promise<FintEvent[]> {
-    const requestUrl = `${API_URL}/event`
+    const requestUrl = `${backendRoutesMap[env.toLowerCase()]}/event`
+    return this.getResponse(requestUrl)
+  }
 
-    const response = await fetch(requestUrl, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.PUBLIC_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    });
+  static async getContracts(env: string): Promise<AdapterContract[]> {
+    const requestUrl = `${backendRoutesMap[env.toLowerCase()]}/contract`
+    return this.getResponse(requestUrl)
+  }
+
+  static async getResponse(requestUrl) {
+    const response = PROFILE === 'local'
+      ? await this.performRequest(requestUrl)
+      : await this.performLocalRequest()
 
     return await response.json()
   }
 
-  static async getContracts(env: string): Promise<AdapterContract[]> {
-    const requestUrl = `${API_URL}/contract`
+  static async performRequest(requestUrl: string) {
+    return await fetch(requestUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${HeaderProperties.getBearerToken()}`,
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 
-    const response = await fetch(requestUrl, {
+  static async performLocalRequest() {
+    return await fetch(process.env.PUBLIC_API_URL, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${process.env.PUBLIC_TOKEN}`,
         'Content-Type': 'application/json',
       },
     });
-
-    return await response.json()
   }
 
 }
