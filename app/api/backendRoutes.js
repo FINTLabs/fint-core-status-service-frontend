6181548971791d5a9ba434b0c98b55835fcfc191
Hknots <hknots99@gmail.com>
2024-12-10 12:21:43 +0100

    add backendRoutes

---
+++ app/api/backendRoutes.js
@@ -0,0 +1,5 @@
+export const backendRoutesMap = {
+    api: "https://core-status.fintlabs.no/api",
+    beta: "https://core-status-beta.fintlabs.no",
+    alpha: "https://core-status-alpha.fintlabs.no"
+};
