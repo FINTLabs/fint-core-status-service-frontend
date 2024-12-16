523abfc5066c3b7107efb4d9ffcabeb41372f7fd
Hknots <hknots99@gmail.com>
2024-12-16 09:43:14 +0100

    move most content to layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,22 +1,14 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useFetcher,
-  useLoaderData,
-} from "@remix-run/react";
+import {Links, Meta, Outlet, Scripts, ScrollRestoration, useFetcher, useLoaderData,} from "@remix-run/react";
 import "./tailwind.css";
 import "@navikt/ds-css";
 import Header from "~/components/root/Header";
-import { Box, HStack, VStack } from "@navikt/ds-react";
-import { LoaderFunctionArgs } from "@remix-run/router";
-import { HeaderProperties } from "~/components/root/HeaderProperties";
-import { envCookie } from "~/components/cookie";
-import { ActionFunction, json } from "@remix-run/node";
+import {Box, HStack, VStack} from "@navikt/ds-react";
+import {LoaderFunctionArgs} from "@remix-run/router";
+import {HeaderProperties} from "~/components/root/HeaderProperties";
+import {envCookie} from "~/components/cookie";
+import {ActionFunction, json} from "@remix-run/node";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export const loader = async ({request}: LoaderFunctionArgs) => {
   HeaderProperties.setProperties(request);
   const cookieHeader = request.headers.get("Cookie");
   let selectedEnv = await envCookie.parse(cookieHeader);
@@ -24,7 +16,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     selectedEnv = "api";
     const newCookieHeader = await envCookie.serialize(selectedEnv);
     return json(
-      { selectedEnv },
+      {selectedEnv},
       {
         headers: {
           "Set-Cookie": newCookieHeader,
@@ -32,41 +24,31 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       }
     );
   }
-  return json({ selectedEnv });
+  return json({selectedEnv});
 };
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export default function App() {
-  const { selectedEnv } = useLoaderData<{ selectedEnv: string }>();
+export function Layout({children}: { children: React.ReactNode }) {
+  const {selectedEnv} = useLoaderData<{ selectedEnv: string }>();
   const fetcher = useFetcher();
 
   function setEnv(env: string) {
     const formData = new FormData();
     formData.append("env", env);
-    fetcher.submit(formData, { method: "POST" });
+    fetcher.submit(formData, {method: "POST"});
   }
 
   return (
+    <html lang="en">
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <Meta/>
+      <Links/>
+    </head>
+    <body>
     <HStack justify="center" className="h-screen">
       <VStack gap="4" justify="center" className="w-3/4 h-full">
-        <Header onHeaderChange={setEnv} value={selectedEnv} />
+        <Header onHeaderChange={setEnv} value={selectedEnv}/>
         <Box
           shadow="small"
           borderRadius="xlarge"
@@ -74,14 +56,24 @@ export default function App() {
           padding="4"
           className="flex-grow w-full px-4 py-6"
         >
-          <Outlet />
+          {children}
         </Box>
       </VStack>
     </HStack>
+    <ScrollRestoration/>
+    <Scripts/>
+    </body>
+    </html>
   );
 }
 
-export const action: ActionFunction = async ({ request }) => {
+export default function App() {
+  return (
+    <Outlet/>
+  );
+}
+
+export const action: ActionFunction = async ({request}) => {
   const formdata = await request.formData();
   const environment = formdata.get("env") as string;
 
@@ -89,7 +81,7 @@ export const action: ActionFunction = async ({ request }) => {
     console.log("environment:", environment);
     const newCookieHeader = await envCookie.serialize(environment);
     return json(
-      { environment },
+      {environment},
       {
         headers: {
           "Set-Cookie": newCookieHeader,
@@ -97,5 +89,5 @@ export const action: ActionFunction = async ({ request }) => {
       }
     );
   }
-  return json({ ok: true });
+  return json({ok: true});
 };
