28c965ac0ac4feb9aa0e096202a77e60e9db3353
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-12-19 16:28:58 +0100

    remove entities

--- app/constants/menu.ts
+++ app/constants/menu.ts
@@ -1,23 +1,19 @@
 type MenuLink = {
-  name: string,
-  href: string
+  name: string;
+  href: string;
 };
 
 export const MENU_LINKS: MenuLink[] = [
   {
-    "name": "Kontrakter",
-    "href": "/kontrakter",
+    name: "Kontrakter",
+    href: "/kontrakter",
   },
   {
-    "name": "Hendelser",
-    "href": "/hendelser",
+    name: "Hendelser",
+    href: "/hendelser",
   },
   {
-    "name": "Entiteter",
-    "href": "/entiteter",
+    name: "Konsumere",
+    href: "/konsumere",
   },
-  {
-    "name": "Konsumere",
-    "href": "/konsumere",
-  },
-]
+];
