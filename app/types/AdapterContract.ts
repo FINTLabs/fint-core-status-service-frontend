003907354a99d6723c2b9a7bb62ace1d2b98d122
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-01-16 11:45:40 +0100

    remove seconds from last activity

--- app/types/AdapterContract.ts
+++ app/types/AdapterContract.ts
@@ -33,9 +33,8 @@ export function convertLastActivity(timestamp: number): string {
 
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
 
-  return `${hours}:${minutes}:${seconds} ${day}/${month}/${year}`;
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
 
 }
 
