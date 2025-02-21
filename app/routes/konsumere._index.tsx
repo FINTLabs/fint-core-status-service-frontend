3e173f283b0b945bcfbc2b70af68ddb0d1d7ef7d
Hknots <hknots99@gmail.com>
2025-02-21 13:25:44 +0100

    refactor: reset konsumer site

--- app/routes/konsumere._index.tsx
+++ app/routes/konsumere._index.tsx
@@ -1,36 +1,7 @@
-import {VStack} from "@navikt/ds-react";
-import ConsumerFilter from "~/components/komponenter/ConsumerFilter";
-import {useState} from "react";
-import FilteredConsumers from "~/components/komponenter/FilteredConsumers";
-import AllConsumers from "~/components/komponenter/AllConsumers";
-import {Consumer} from "~/components/komponenter/Consumer";
-import Index from "~/routes/_index";
-
 export default function Konsumere() {
-    const [filter, setFilter]: boolean = useState(false)
-
-    function createDummyConsumers(): Consumer[] {
-        const lol = []
-        lol[0] = {
-            domain: "Utdanning",
-            package: "Vurdering",
-            org: "fintlabs.no",
-            cacheSize: 10,
-            targetCacheSize: 10
-        }
-        return lol
-    }
-
-    const consumers = createDummyConsumers()
-    return (
-        <div>
-            <div>
-                <Index />
-            </div>
-        <VStack justify="center" margin="8" gap="4">
-            <ConsumerFilter/>
-            {filter ? <FilteredConsumers/> : <AllConsumers consumers={consumers}/>}
-        </VStack>
-        </div>
-    );
+  return (
+    <div>
+      test
+    </div>
+  );
 }
