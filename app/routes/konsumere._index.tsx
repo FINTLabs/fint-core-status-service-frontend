b0036bcf2da815bca6a44f7c3412833d17e598d6
Hknots <hknots99@gmail.com>
2025-02-25 13:27:37 +0100

    feat: setup search & buttons

--- app/routes/konsumere._index.tsx
+++ app/routes/konsumere._index.tsx
@@ -1,7 +1,40 @@
+import {ActionMenu, Button, HStack, Search, VStack} from "@navikt/ds-react";
+import {ArrowsUpDownIcon, ChevronDownIcon, PencilIcon, PlusIcon} from "@navikt/aksel-icons";
+import React from "react";
+
 export default function Konsumere() {
+  const [views, setViews] = React.useState({
+    started: true,
+    fnr: false,
+    tags: true,
+  });
+
+  const [rows, setRows] = React.useState<string>("5");
+
+  const handleCheckboxChange = (checkboxId: string) => {
+    setViews((prevState) => ({
+      ...prevState,
+      [checkboxId]: !prevState[checkboxId],
+    }));
+  };
+
   return (
-    <div>
-      test
-    </div>
+    <VStack className="bg-gray-200">
+      <HStack justify="space-between">
+        <HStack gap="2">
+          <form role="search">
+            <Search label="Søk alle Nav sine sider" variant="primary"/>
+          </form>
+        </HStack>
+        <HStack gap="2">
+          <Button iconPosition="right" icon={<ArrowsUpDownIcon aria-hidden/>}>
+          Oppdater Versjon
+          </Button>
+          <Button iconPosition="right" icon={<PlusIcon aria-hidden/>}>
+            Ny Consumer
+          </Button>
+        </HStack>
+      </HStack>
+    </VStack>
   );
 }
