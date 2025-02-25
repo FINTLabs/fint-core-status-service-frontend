9fc9aad45af289dc2e51206b92a5dea99d5fbd06
Hknots <hknots99@gmail.com>
2025-02-25 15:12:44 +0100

    feat: create dummy data for konsumerTabs

---
+++ app/mocks/konsumerTabs.ts
@@ -0,0 +1,15 @@
+import {IKonsumerTab} from "~/types/IKonsumerTab";
+
+export const konsumerTabs: IKonsumerTab[] = [
+  {org: "Oslo", applications: 25, errors: 0, restarts: 0},
+  {org: "Viken", applications: 30, errors: 2, restarts: 0},
+  {org: "Innlandet", applications: 18, errors: 0, restarts: 0},
+  {org: "Vestfold og Telemark", applications: 20, errors: 0, restarts: 0},
+  {org: "Agder", applications: 12, errors: 0, restarts: 0},
+  {org: "Rogaland", applications: 22, errors: 0, restarts: 0},
+  {org: "Vestland", applications: 16, errors: 0, restarts: 0},
+  {org: "Møre og Romsdal", applications: 14, errors: 0, restarts: 0},
+  {org: "Trøndelag", applications: 28, errors: 0, restarts: 1},
+  {org: "Nordland", applications: 19, errors: 0, restarts: 0},
+  {org: "Troms og Finnmark", applications: 11, errors: 0, restarts: 0},
+];
