8e188456483e5efb92fec33fb48514227b04b757
Hknots <hknots99@gmail.com>
2025-02-27 13:51:55 +0100

    feat: add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,11 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      header: "#1F3D59",
+      body: "#ECEBF5",
+      "tab-body": "#98B1C9",
+    },
   },
   plugins: [],
 } satisfies Config;
