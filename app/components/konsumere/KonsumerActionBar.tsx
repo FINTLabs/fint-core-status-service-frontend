5adc7078083f7fc66dab6d4c99d2fda92c73903f
Hknots <hknots99@gmail.com>
2025-02-27 13:51:04 +0100

    chore: not sure whats changed rofl

--- app/components/konsumere/KonsumerActionBar.tsx
+++ app/components/konsumere/KonsumerActionBar.tsx
@@ -3,21 +3,21 @@ import {ArrowsUpDownIcon, PlusIcon} from "@navikt/aksel-icons";
 import React from "react";
 
 export default function KonsumerActionBar() {
-    return (
-      <HStack justify="space-between">
-        <HStack gap="2">
-          <form role="search">
-            <Search label="Søk alle Nav sine sider" variant="primary"/>
-          </form>
-        </HStack>
-        <HStack gap="2">
-          <Button iconPosition="right" icon={<ArrowsUpDownIcon aria-hidden/>}>
-            Oppdater Versjon
-          </Button>
-          <Button iconPosition="right" icon={<PlusIcon aria-hidden/>}>
-            Ny Consumer
-          </Button>
-        </HStack>
+  return (
+    <HStack justify="space-between">
+      <HStack gap="2">
+        <form role="search">
+          <Search label="Søk alle Nav sine sider" variant="primary"/>
+        </form>
       </HStack>
-    )
+      <HStack gap="2">
+        <Button iconPosition="right" icon={<ArrowsUpDownIcon aria-hidden/>}>
+          Oppdater Versjon
+        </Button>
+        <Button iconPosition="right" icon={<PlusIcon aria-hidden/>}>
+          Ny Consumer
+        </Button>
+      </HStack>
+    </HStack>
+  )
 }
