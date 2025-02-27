c57d8711592ab9b799c5737c73bbb11801483194
Hknots <hknots99@gmail.com>
2025-02-27 14:56:22 +0100

    feat: setup konsumere page

--- app/routes/konsumere._index.tsx
+++ app/routes/konsumere._index.tsx
@@ -2,16 +2,21 @@ import {HStack, VStack} from "@navikt/ds-react";
 import React from "react";
 import KonsumerActionBar from "~/components/konsumere/KonsumerActionBar";
 import KonsumerTab from "~/components/konsumere/KonsumerTab";
+import Title from "~/components/header/Title";
+import {konsumerTabs} from "~/mocks/konsumerTabs";
 
 export default function Konsumere() {
   return (
-    <VStack className="bg-gray-200" gap="4" padding="4">
-      <KonsumerActionBar/>
-      <HStack gap="2">
-        <KonsumerTab/>
-        <KonsumerTab/>
-        <KonsumerTab/>
-      </HStack>
-    </VStack>
+    <HStack justify="center">
+      <VStack className="w-2/3" gap="8">
+        <Title/>
+        <KonsumerActionBar/>
+        <HStack gap="4">
+          {konsumerTabs.map((tab) => (
+            <KonsumerTab org={tab.org} applications={tab.applications} errors={tab.errors} restarts={tab.errors} />
+          ))}
+        </HStack>
+      </VStack>
+    </HStack>
   );
 }
