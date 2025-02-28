2e0d55250626e23d5214fe7da3b3db1a8c81c05c
Hknots <hknots99@gmail.com>
2025-02-28 12:02:39 +0100

    feat: add AdjustConsumerModal

---
+++ app/components/konsumere/add_konsumer/AdjustConsumerModal.tsx
@@ -0,0 +1,72 @@
+import {HStack, Modal, Stepper, UNSAFE_Combobox, VStack} from "@navikt/ds-react";
+import {PlusIcon} from "@navikt/aksel-icons";
+import React, {useEffect, useState} from "react";
+import SetupFields from "~/components/konsumere/add_konsumer/SetupFields";
+import {emptyConsumer, IConsumer} from "~/types/IConsumer";
+import {IConsumerMetadata} from "~/types/IConsumerMetadata";
+
+interface AdjustConsumerModalProps {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  consumerMetadata: IConsumerMetadata
+  consumer: IConsumer | null
+}
+
+export default function AdjustConsumerModal({open, setOpen, consumerMetadata, consumer = null}: AdjustConsumerModalProps) {
+  const [activeStep, setActiveStep] = useState(1);
+  const [consumer, setConsumer]: IConsumer = useState(emptyConsumer)
+
+  useEffect(() => {
+    console.log("Updated consumer: ", consumer)
+  }, [consumer])
+
+  const requiredFieldsIsSet = () => {
+    return consumer.org && consumer.domain && consumer.package && consumer.version
+  }
+
+  const changeStep = (step) => {
+    if (activeStep === 1 && !requiredFieldsIsSet()) {
+      console.log("Required fields are not set!")
+      console.log(consumer)
+      return
+    }
+    setActiveStep(step)
+  }
+
+  return (
+    <form>
+      <Modal
+        width="700px"
+        open={open}
+        onClose={() => setOpen(false)}
+        header={{
+          heading: "Ny Konsumer",
+          icon: <PlusIcon/>
+        }}
+      >
+        <Modal.Body>
+          <VStack padding="2" justify="center" gap="4">
+            <HStack justify="center" gap="2">
+              {activeStep === 1 && <SetupFields setConsumer={setConsumer} />}
+            </HStack>
+            <HStack justify="space-between">
+            </HStack>
+          </VStack>
+        </Modal.Body>
+        <Modal.Footer className="flex justify-center">
+          <Stepper
+            aria-labelledby="stepper-heading"
+            activeStep={activeStep}
+            onStepChange={changeStep}
+            orientation="horizontal"
+          >
+            <Stepper.Step href="#">Oppsett</Stepper.Step>
+            <Stepper.Step href="#">Ressurser</Stepper.Step>
+            <Stepper.Step href="#">Alokering</Stepper.Step>
+            <Stepper.Step href="#">Bekreftelse</Stepper.Step>
+          </Stepper>
+        </Modal.Footer>
+      </Modal>
+    </form>
+  )
+}
