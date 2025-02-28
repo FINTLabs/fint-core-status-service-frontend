9081a1813dce288b54c24a80c9bd4695ab57087b
Hknots <hknots99@gmail.com>
2025-02-28 13:03:40 +0100

    refactor: replace open useState with ref to open modals

--- app/routes/konsumere._index.tsx
+++ app/routes/konsumere._index.tsx
@@ -1,21 +1,21 @@
 import {HStack, VStack} from "@navikt/ds-react";
-import React, {useState} from "react";
-import KonsumerActionBar from "~/components/konsumere/KonsumerActionBar";
+import React, {useRef, useState} from "react";
+import ConsumerActionbar from "~/components/konsumere/ConsumerActionbar";
 import ConsumerTab from "~/components/konsumere/ConsumerTab";
 import Title from "~/components/header/Title";
 import {MockConsumerTabs} from "~/mocks/mock_consumer_tabs";
 import AdjustConsumerModal from "~/components/konsumere/add_konsumer/AdjustConsumerModal";
-import {IConsumer} from "~/types/IConsumer";
-import {IConsumerMetadata} from "~/types/IConsumerMetadata";
 import {MockConsumerMetadata} from "~/mocks/mock_consumer_metadata";
+import {emptyConsumer} from "~/types/IConsumer";
 
 export default function Konsumere() {
-  const [tabs] = useState(MockConsumerTabs)
+  const ref = useRef<HTMLDialogElement>(null);
+  const [consumerTabs] = useState(MockConsumerTabs)
+  const [consumer, setConsumer] = useState(emptyConsumer)
   const [searchQuery, setSearchQuery] = useState("");
-  const [openConsumerEditModal, setOpenConsumerEditModal] = useState(false)
-  const [consumer, setConsumer]: IConsumer = useState(null)
+  const [consumerMetadata, setConsumerMetadata] = useState(MockConsumerMetadata)
 
-  const filteredTabs = tabs.filter(tab =>
+  const filteredTabs = consumerTabs.filter(tab =>
     tab.org.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
@@ -23,9 +23,9 @@ export default function Konsumere() {
     <HStack justify="center">
       <VStack className="w-2/3" gap="8">
         <Title/>
-        <KonsumerActionBar
+        <ConsumerActionbar
           setQuery={setSearchQuery}
-          setOpenAddConsumer={setOpenConsumerEditModal}
+          ref={ref}
         />
         <HStack gap="4">
           {filteredTabs.map((tab) => (
@@ -40,11 +40,11 @@ export default function Konsumere() {
         </HStack>
 
         <AdjustConsumerModal
-          open={openConsumerEditModal}
-          setOpen={setOpenConsumerEditModal}
+          ref={ref}
+          consumerMetadata={consumerMetadata}
           consumer={consumer}
           setConsumer={setConsumer}
-          consumerMetadata={consumerMetadata}
+          existingConsumer={false}
         />
       </VStack>
     </HStack>
