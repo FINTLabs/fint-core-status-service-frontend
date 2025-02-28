27a6834df35107d33689435695fcdeadbfb87322
Hknots <hknots99@gmail.com>
2025-02-28 12:04:02 +0100

    refactor/feat: implement AdjustConsumerModal & adjust renames

--- app/routes/konsumere._index.tsx
+++ app/routes/konsumere._index.tsx
@@ -1,13 +1,21 @@
 import {HStack, VStack} from "@navikt/ds-react";
 import React, {useState} from "react";
 import KonsumerActionBar from "~/components/konsumere/KonsumerActionBar";
-import KonsumerTab from "~/components/konsumere/KonsumerTab";
+import ConsumerTab from "~/components/konsumere/ConsumerTab";
 import Title from "~/components/header/Title";
-import {konsumerTabs} from "~/mocks/konsumerTabs";
+import {MockConsumerTabs} from "~/mocks/mock_consumer_tabs";
+import AdjustConsumerModal from "~/components/konsumere/add_konsumer/AdjustConsumerModal";
+import {IConsumer} from "~/types/IConsumer";
+import {IConsumerMetadata} from "~/types/IConsumerMetadata";
+import {MockConsumerMetadata} from "~/mocks/mock_consumer_metadata";
 
 export default function Konsumere() {
+  const [tabs] = useState(MockConsumerTabs)
   const [searchQuery, setSearchQuery] = useState("");
-  const [tabs] = useState(konsumerTabs)
+  const [openConsumerEditModal, setOpenConsumerEditModal] = useState(false)
+  const [consumer, setConsumer]: IConsumer = useState(null)
+  const [consumerMetadata, consumerMetadata]: IConsumerMetadata = useState(MockConsumerMetadata)
+
   const filteredTabs = tabs.filter(tab =>
     tab.org.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -16,10 +24,13 @@ export default function Konsumere() {
     <HStack justify="center">
       <VStack className="w-2/3" gap="8">
         <Title/>
-        <KonsumerActionBar setQuery={setSearchQuery}/>
+        <KonsumerActionBar
+          setQuery={setSearchQuery}
+          setOpenAddConsumer={setOpenConsumerEditModal}
+        />
         <HStack gap="4">
           {filteredTabs.map((tab) => (
-            <KonsumerTab
+            <ConsumerTab
               key={tab.org}
               org={tab.org}
               applications={tab.applications}
@@ -28,6 +39,14 @@ export default function Konsumere() {
             />
           ))}
         </HStack>
+
+        <AdjustConsumerModal
+          open={openConsumerEditModal}
+          setOpen={setOpenConsumerEditModal}
+          consumer={consumer}
+          setConsumer={setConsumer}
+          consumerMetadata={consumerMetadata}
+        />
       </VStack>
     </HStack>
   );
