edf8f46309fc24df48de8c03160ff1290cda1ee1
Hknots <hknots99@gmail.com>
2025-03-03 10:54:02 +0100

    feat: add organisation site

---
+++ app/routes/konsumere.$org.tsx
@@ -0,0 +1,24 @@
+import {HStack, VStack} from "@navikt/ds-react";
+import React, {useState} from "react";
+import ConsumerActionbar from "~/components/konsumere/ConsumerActionbar";
+import Title from "~/components/header/Title";
+import {MockOrganisationTabs} from "~/mocks/mock_organisation_tabs";
+
+export default function Konsumere() {
+  const [consumerTabs] = useState(MockOrganisationTabs)
+  const [openModal, setOpenModal] = useState(false)
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <HStack justify="center">
+      <VStack className="w-2/3" gap="8">
+        <Title/>
+        <ConsumerActionbar
+          setQuery={setSearchQuery}
+          setOpenModal={setOpenModal}
+        />
+
+      </VStack>
+    </HStack>
+  );
+}
