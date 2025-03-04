f64961fe66a218ab4acb8a1dc1e0fb16dd73aa96
Hknots <hknots99@gmail.com>
2025-03-04 13:34:20 +0100

    feat: add felles toggle

--- app/components/konsumere/konsumer_modal/SetupFields.tsx
+++ app/components/konsumere/konsumer_modal/SetupFields.tsx
@@ -1,19 +1,14 @@
-import {Select, UNSAFE_Combobox} from "@navikt/ds-react";
+import {Box, HStack, Select, Switch, Tooltip, UNSAFE_Combobox, VStack} from "@navikt/ds-react";
 import {IConsumer} from "~/types/IConsumer";
 import {IConsumerMetadata} from "~/types/IConsumerMetadata";
-import {useEffect, useState} from "react";
 
 interface SetupFieldsProps {
-  setConsumer: React.Dispatch<React.SetStateAction<IConsumer>>,
+  consumer: IConsumer
+  setConsumer: React.Dispatch<React.SetStateAction<IConsumer>>
   consumerMetadata: IConsumerMetadata
 }
 
-export default function SetupFields({setConsumer, consumerMetadata}: SetupFieldsProps) {
-
-  useEffect(() => {
-    console.log("User state changed:", organisations);
-  }, [organisations]);
-
+export default function SetupFields({consumer, setConsumer, consumerMetadata}: SetupFieldsProps) {
   const handleChange = (field: keyof IConsumer, value: string | boolean | string[]) => {
     setConsumer((prev) => ({
       ...prev,
@@ -21,33 +16,53 @@ export default function SetupFields({setConsumer, consumerMetadata}: SetupFields
     }));
   };
 
-  const handleSelection = (setter, option, isSelected) => {
-    setter(prev =>
-      isSelected
-        ? [...prev, option]
-        : prev.filter(o => o !== option)
-    )
+  const updateListField = (
+    field: keyof Pick<IConsumer, "organisations" | "components">,
+    option: string,
+    isSelected: boolean
+  ) => {
+    setConsumer((prev) => ({
+      ...prev,
+      [field]: isSelected
+        ? [...prev[field], option]
+        : prev[field].filter((o: string) => o !== option),
+    }));
   }
 
   return (
     <>
-      <UNSAFE_Combobox
-        isMultiSelect
-        selected
-        label="Velg organisasjoner"
-        options={consumerMetadata.orgs}
-        selectedOptions={organisations}
-        onToggleSelected={(option, isSelected) => handleSelection(setOrganisations, option, isSelected)}
-      />
-      <UNSAFE_Combobox
-        isMultiSelect
-        selected
-        label="Velg Komponent"
-        options={consumerMetadata.components}
-        selectedOptions={organisations}
-        onToggleSelected={(option, isSelected) => handleSelection(setOrganisations, option, isSelected)}
-      />
+      <HStack justify="space-between" gap="2" wrap={false}>
+        <UNSAFE_Combobox
+          className="w-full"
+          isMultiSelect
+          selected
+          label="Velg organisasjoner"
+          options={consumerMetadata.orgs}
+          selectedOptions={consumer.organisations}
+          onToggleSelected={(option, isSelected) => updateListField("organisations", option, isSelected)}
+          disabled={consumer.shared}
+        />
+        <VStack justify="center">
+          <HStack gap="2" className="pt-8">
+            <Tooltip content="Alle organisasjoner deler samme consumer">
+              <Switch onChange={e => handleChange("shared", e.target.checked)}>Felles</Switch>
+            </Tooltip>
+          </HStack>
+        </VStack>
+      </HStack>
+      <HStack gap="2" wrap={false}>
+        <UNSAFE_Combobox
+          className="w-full"
+          isMultiSelect
+          selected
+          label="Velg Komponent"
+          options={consumerMetadata.components}
+          selectedOptions={consumer.components}
+          onToggleSelected={(option, isSelected) => updateListField("components", option, isSelected)}
+        />
+      </HStack>
       <Select
+        className="w-44"
         hideLabel
         label="Velg Versjon"
         onChange={(e) => handleChange("version", e.target.value)}
