ed4f12f8ff990d4d585ff55f914c6a9f4bfb4505
Hknots <hknots99@gmail.com>
2025-03-04 13:34:39 +0100

    refactor: adjust properties and fields

--- app/routes/konsumere._index.tsx
+++ app/routes/konsumere._index.tsx
@@ -6,29 +6,27 @@ import Title from "~/components/header/Title";
 import {MockOrganisationTabs} from "~/mocks/mock_organisation_tabs";
 import ConsumerModal from "~/components/konsumere/konsumer_modal/ConsumerModal";
 import {MockConsumerMetadata} from "~/mocks/mock_consumer_metadata";
-import {emptyConsumer} from "~/types/IConsumer";
 
 export default function Konsumere() {
   const [openModal, setOpenModal] = useState(false)
-  const [consumerTabs] = useState(MockOrganisationTabs)
-  const [consumer, setConsumer] = useState(emptyConsumer)
-  const [searchQuery, setSearchQuery] = useState("");
-  const [consumerMetadata, setConsumerMetadata] = useState(MockConsumerMetadata)
+  const [query, setQuery] = useState("")
+  const [organisationTabs] = useState(MockOrganisationTabs)
+  const [consumerMetadata] = useState(MockConsumerMetadata)
 
-  const filteredTabs = consumerTabs.filter(tab =>
-    tab.org.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredOrganisationTabs = organisationTabs.filter(tab =>
+    tab.org.toLowerCase().includes(query.toLowerCase())
+  )
 
   return (
     <HStack justify="center">
       <VStack className="w-2/3" gap="8">
         <Title/>
         <ConsumerActionbar
-          setQuery={setSearchQuery}
-          setOpenModal={setOpenModal}
+          setQuery={setQuery}
+          openModalSetter={setOpenModal}
         />
         <HStack gap="4">
-          {filteredTabs.map((tab) => (
+          {filteredOrganisationTabs.map((tab) => (
             <OrganisationTab
               key={tab.org}
               org={tab.org}
@@ -43,9 +41,6 @@ export default function Konsumere() {
           openModal={openModal}
           setOpenModal={setOpenModal}
           consumerMetadata={consumerMetadata}
-          consumer={consumer}
-          setConsumer={setConsumer}
-          existingConsumer={false}
         />
       </VStack>
     </HStack>
