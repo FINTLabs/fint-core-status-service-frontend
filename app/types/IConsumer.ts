acfcfe670507f203918411ec4cb83b4b276ad99e
Hknots <hknots99@gmail.com>
2025-03-07 10:38:50 +0100

    refactor: adjust limits and request fields to own interface

--- app/types/IConsumer.ts
+++ app/types/IConsumer.ts
@@ -1,14 +1,24 @@
 import {IResource} from "~/types/IComponent";
 
+export type MemoryUnit = "Mi" | "Gi";
+export type CpuUnit = "mm" | "core";
+
+export interface IAllocation<T extends string> {
+  request: string;
+  requestType: T;
+  limit: string;
+  limitType: T;
+}
+
 export interface IConsumer {
-  components: Record<string, IResource[]>
+  components: Record<string, IResource[]>;
   organisations: string[];
   version: string;
   shared: boolean;
-  limitsCpu: string;
-  limitsMemory: string;
-  requestsCpu: string;
-  requestsMemory: string;
+  allocations: {
+    memory: IAllocation<MemoryUnit>;
+    cpu: IAllocation<CpuUnit>;
+  };
   writeableResources: string[];
   cacheDisabledResources: string[];
 }
@@ -18,10 +28,20 @@ export const emptyConsumer: IConsumer = {
   organisations: [],
   version: "",
   shared: false,
-  limitsCpu: "500m",
-  limitsMemory: "512Mi",
-  requestsCpu: "100m",
-  requestsMemory: "256Mi",
+  allocations: {
+    memory: {
+      request: "256",
+      requestType: "Mi",
+      limit: "512",
+      limitType: "Mi"
+    },
+    cpu: {
+      request: "100",
+      requestType: "mm",
+      limit: "500",
+      limitType: "mm"
+    }
+  },
   writeableResources: [],
   cacheDisabledResources: []
 };
