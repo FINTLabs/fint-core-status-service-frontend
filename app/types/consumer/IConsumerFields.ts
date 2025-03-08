f17bf8f547636135b9aa14f49d2d2301da3d11db
Hknots <hknots99@gmail.com>
2025-03-08 20:56:44 +0100

    refactor: add organisations and components

--- app/types/consumer/IConsumerFields.ts
+++ app/types/consumer/IConsumerFields.ts
@@ -1,19 +1,29 @@
 import {IConsumerRequest} from "~/types/consumer/IConsumerRequest";
-import {allocationsFromRequest, CpuUnit, defaultAllocations, IAllocation, MemoryUnit} from "~/types/consumer/IAllocation";
+import {
+  allocationsFromRequest,
+  CpuUnit,
+  defaultAllocations,
+  IAllocation,
+  MemoryUnit
+} from "~/types/consumer/IAllocation";
 
 export interface IConsumerFields {
   version: string;
   shared: boolean;
+  organisations: string[];
+  components: string[];
   allocations: {
     memory: IAllocation<MemoryUnit>;
     cpu: IAllocation<CpuUnit>;
   };
 }
 
-export function newConsumerFields() {
+export function newConsumerFields(): IConsumerFields {
   return {
     version: "",
     shared: false,
+    organisations: [],
+    components: [],
     allocations: defaultAllocations()
   }
 }
@@ -22,6 +32,8 @@ export function consumerFieldsFromRequest(consumerRequest: IConsumerRequest): IC
   return {
     version: consumerRequest.version,
     shared: true,
+    organisations: [consumerRequest.org],
+    components: [`${consumerRequest.domain} ${consumerRequest.package}`],
     allocations: allocationsFromRequest(consumerRequest)
   }
 }
