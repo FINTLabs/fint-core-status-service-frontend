c2c5a6ab98721f841c18ecede95e427f7482a93d
Hknots <hknots99@gmail.com>
2025-03-08 22:45:00 +0100

    refactor: structure modal body differently & adjust properties

--- app/components/konsumere/konsumer_modal/ConsumerModal.tsx
+++ app/components/konsumere/konsumer_modal/ConsumerModal.tsx
@@ -2,43 +2,60 @@ import {HStack, Modal, Stepper, VStack} from "@navikt/ds-react";
 import {PlusIcon} from "@navikt/aksel-icons";
 import React, {useState} from "react";
 import SetupPage from "~/components/konsumere/konsumer_modal/SetupPage";
-import {IConsumer} from "~/types/IConsumer";
-import {IConsumerMetadata} from "~/types/IConsumerMetadata";
+import {consumerFromRequest, newConsumer} from "~/types/consumer/IConsumer";
+import {IConsumerMetadata} from "~/types/consumer/IConsumerMetadata";
 import ResourcePage from "~/components/konsumere/konsumer_modal/ResourcePage";
-import {mockConsumer} from "~/mocks/mock_consumer";
 import AllocationPage from "~/components/konsumere/konsumer_modal/AllocationPage";
 import ConfirmationPage from "~/components/konsumere/konsumer_modal/ConfirmationPage";
+import {IConsumerRequest} from "~/types/consumer/IConsumerRequest";
 
 interface AdjustConsumerModalProps {
   openModal: boolean
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>
-  initialConsumer: IConsumer
   consumerMetadata: IConsumerMetadata
   editing: boolean
+  initialConsumer?: IConsumerRequest
 }
 
 export default function ConsumerModal({
                                         openModal,
                                         setOpenModal,
-                                        initialConsumer = mockConsumer,
+                                        consumerMetadata,
+                                        initialConsumer,
                                         editing = false,
-                                        consumerMetadata
                                       }: AdjustConsumerModalProps) {
-  const [activeStep, setActiveStep] = useState(4);
-  const [consumer, setConsumer] = useState(initialConsumer)
+  const [activeStep, setActiveStep] = useState(1);
+  const [consumerFields, setConsumerFields] = useState(
+    initialConsumer != undefined
+      ? consumerFromRequest(initialConsumer)
+      : newConsumer()
+  )
 
-  const requiredFieldsIsSet = () =>
-    requiredOrganisationSelectionIsSet() && consumer.components.length && consumer.version || false;
-
-
-  const requiredOrganisationSelectionIsSet = () => {
-    return consumer.shared || consumer.organisations.length !== 0;
-  }
-
-  const changeStep = step =>
-    activeStep === 1 && !requiredFieldsIsSet()
-      ? console.log("Required fields are not set!")
-      : setActiveStep(step)
+  const steps = [
+    <SetupPage
+      key="setup"
+      editing={editing}
+      consumerMetadata={consumerMetadata}
+      consumer={consumerFields}
+      setConsumer={setConsumerFields}
+    />,
+    <ResourcePage
+      key="resource"
+      consumer={consumerFields}
+      setConsumer={setConsumerFields}
+    />,
+    <AllocationPage
+      key="allocation"
+      consumer={consumerFields}
+      setConsumer={setConsumerFields}
+    />,
+    <ConfirmationPage
+      key="confirmation"
+      editing={editing}
+      initialConsumer={initialConsumer}
+      consumer={consumerFields}
+    />
+  ];
 
   return (
     <form>
@@ -52,21 +69,14 @@ export default function ConsumerModal({
         }}
       >
         <Modal.Body>
-          <VStack padding="2" justify="center" gap="6">
-            {activeStep === 1 &&
-                <SetupPage editing={editing} consumerMetadata={consumerMetadata} consumer={consumer} setConsumer={setConsumer}/>}
-            {activeStep === 2 && <ResourcePage consumer={consumer} setConsumer={setConsumer}/>}
-            {activeStep === 3 && <AllocationPage consumer={consumer} setConsumer={setConsumer}/>}
-            {activeStep === 4 && <ConfirmationPage/>}
-          </VStack>
+          {steps[activeStep - 1]}
         </Modal.Body>
         <Modal.Footer className="flex justify-center">
           <HStack>
-            <div>1</div>
             <Stepper
               aria-labelledby="stepper-heading"
               activeStep={activeStep}
-              onStepChange={changeStep}
+              onStepChange={step => setActiveStep(step)}
               orientation="horizontal"
             >
               <Stepper.Step href="#">Oppsett</Stepper.Step>
@@ -74,7 +84,6 @@ export default function ConsumerModal({
               <Stepper.Step href="#">Ytelse</Stepper.Step>
               <Stepper.Step href="#">Bekreftelse</Stepper.Step>
             </Stepper>
-            <div>1</div>
           </HStack>
         </Modal.Footer>
       </Modal>
