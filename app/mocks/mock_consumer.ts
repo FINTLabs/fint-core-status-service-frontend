a611df15ef215e1bbb772404c9bdc91039d04a16
Hknots <hknots99@gmail.com>
2025-03-10 22:16:13 +0100

    refactor: adjust mock data acording to IConsumerRequest

--- app/mocks/mock_consumer.ts
+++ app/mocks/mock_consumer.ts
@@ -1,40 +1,16 @@
-import {IConsumer} from "~/types/IConsumer";
+import {IConsumerRequest} from "~/types/consumer/IConsumerRequest";
 
-export const mockConsumer: IConsumer = {
-  components: {
-    "utdanning vurdering": [
-      {name: "Resource A", enabled: false, writeable: false, cacheDisabled: false},
-      {name: "Resource B", enabled: false, writeable: false, cacheDisabled: false},
-      {name: "Resource C", enabled: false, writeable: true, cacheDisabled: false}
-    ],
-    "utdanning elev": [
-      {name: "Resource D", enabled: false, writeable: false, cacheDisabled: false},
-      {name: "Resource E", enabled: false, writeable: true, cacheDisabled: false},
-      {name: "Resource F", enabled: false, writeable: false, cacheDisabled: false}
-    ],
-    "utdanning utdanningsprogram": [
-      {name: "Resource G", enabled: false, writeable: false, cacheDisabled: false},
-      {name: "Resource H", enabled: false, writeable: false, cacheDisabled: false},
-      {name: "Resource I", enabled: false, writeable: false, cacheDisabled: false}
-    ]
-  },
-  organisations: [],
-  version: "",
-  shared: false,
-  allocations: {
-    memory: {
-      request: "256",
-      requestType: "Mi",
-      limit: "512",
-      limitType: "Mi"
-    },
-    cpu: {
-      request: "100",
-      requestType: "mm",
-      limit: "500",
-      limitType: "mm"
-    }
-  },
-  writeableResources: [],
+export const mockConsumerRequest: IConsumerRequest = {
+  domain: "utdanning",
+  package: "vurdering",
+  version: "3.19.0",
+  org: "fintlabs.no",
+  shared: true,
+  limitsCpu: "500m",
+  limitsMemory: "512Mi",
+  requestsCpu: "100m",
+  requestsMemory: "256Mi",
+  resources: ["Resource A", "Resource B", "Resource C"],
+  writeableResources: ["Resource C"],
   cacheDisabledResources: []
 };
