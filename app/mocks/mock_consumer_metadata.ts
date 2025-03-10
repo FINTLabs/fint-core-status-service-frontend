2080ee0a1464d2ae50e708bd47664fe409d26600
Hknots <hknots99@gmail.com>
2025-03-10 22:16:21 +0100

    refactor: rename field

--- app/mocks/mock_consumer_metadata.ts
+++ app/mocks/mock_consumer_metadata.ts
@@ -1,7 +1,7 @@
-import {IConsumerMetadata} from "~/types/IConsumerMetadata";
+import {IConsumerMetadata} from "~/types/consumer/IConsumerMetadata";
 
 export const MockConsumerMetadata: IConsumerMetadata = {
-  orgs: ["oslo.no", "vestland.no", "viken.no", "innlandet.no", "trøndelag.no"],
+  organisations: ["oslo.no", "vestland.no", "viken.no", "innlandet.no", "trøndelag.no"],
   versions: ["3.19.0", "3.19.5", "3.19.7", "3.19.10", "3.19.12"],
   components: ["utdanning vurdering", "administrasjon personal"],
 }
