5d9d540a701e850a5a0cb627f147c9c03e039b3a
Hknots <hknots99@gmail.com>
2025-03-11 19:19:13 +0100

    feat: set up main structure of deploy page

--- app/components/konsumere/konsumer_modal/DeployPage.tsx
+++ app/components/konsumere/konsumer_modal/DeployPage.tsx
@@ -1,7 +1,33 @@
-export default function DeployPage() {
+import {Box, Button, Heading, HStack, Loader, VStack} from "@navikt/ds-react";
+import {IConsumer} from "~/types/consumer/IConsumer";
+
+interface DeployPageProps {
+  consumer: IConsumer
+  editing: boolean
+}
+
+export default function DeployPage({consumer, editing}: DeployPageProps) {
   return (
-    <>
-      hi
-    </>
+    <VStack gap="2">
+      <HStack>
+        <Button>
+          Deploy
+        </Button>
+      </HStack>
+      <Box
+        className="w-full flex justify-between p-2 h-12"
+        shadow="medium"
+        borderRadius="large"
+      >
+        <VStack justify="center">
+          <HStack gap="4">
+            <Loader size="medium" title="Venter..."/>
+            <Heading size="small">
+              Utdanning utdanningsprogram - fintlabs.no
+            </Heading>
+          </HStack>
+        </VStack>
+      </Box>
+    </VStack>
   )
 }
