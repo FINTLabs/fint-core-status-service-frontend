6efdc07bad0ac7762994a822a0c761cfb8010414
Hknots <hknots99@gmail.com>
2025-03-14 10:16:23 +0100

    refactor: implement Tab

--- app/components/konsumere/OrganisationTab.tsx
+++ app/components/konsumere/OrganisationTab.tsx
@@ -1,62 +1,34 @@
-import {Box, HStack, VStack} from "@navikt/ds-react";
 import {ArrowsCirclepathIcon, CaretRightCircleIcon, XMarkOctagonIcon} from "@navikt/aksel-icons";
 import React from "react";
-import {IOrganisationTab} from "~/types/IOrganisationTab";
-import clsx from "clsx";
+import {Tab, TabField} from "~/components/konsumere/Tab";
 
-interface onClickProp {
-  org: string,
-  applications: number,
-  errors: number,
-  restarts: number,
-  onClick?: () => void
-  className?: string
+interface OrganizationTabProps {
+  org: string;
+  applications: number;
+  errors: number;
+  restarts: number;
+  onClick?: () => void;
+  className?: string;
 }
 
-export default function OrganisationTab({
-                                          org,
-                                          applications,
-                                          errors,
-                                          restarts,
-                                          onClick,
-                                          className
-                                        }: IOrganisationTab & onClickProp) {
-  return (
-    <Box
-      borderRadius="large"
-      shadow="large"
-      width="200px"
-      className={clsx("flex flex-col bg-[rgb(152,177,201)]", className)}
-      onClick={onClick}
-    >
-      <Box
-        className="text-center bg-header text-body"
-        padding="4"
-        borderRadius="large"
-        width="200px"
-      >
-        {org}
-      </Box>
-      <Box
-        className="bg-"
-        borderRadius="0 0 large large"
-        padding="1"
-      >
-        <VStack gap="1">
-          <HStack gap="2" align="center">
-            <CaretRightCircleIcon aria-hidden/>
-            {applications}
-          </HStack>
-          <HStack gap="2" align="center">
-            <XMarkOctagonIcon aria-hidden/>
-            {errors}
-          </HStack>
-          <HStack gap="2" align="center">
-            <ArrowsCirclepathIcon aria-hidden/>
-            {restarts}
-          </HStack>
-        </VStack>
-      </Box>
-    </Box>
-  )
+export function OrganizationTab({
+                                  org,
+                                  applications,
+                                  errors,
+                                  restarts,
+                                  onClick,
+                                  className,
+                                }: OrganizationTabProps) {
+  const fields: TabField[] = [
+    {icon: <CaretRightCircleIcon aria-hidden/>, value: applications, toolTip: "Antall konsumere"},
+    {icon: <XMarkOctagonIcon aria-hidden/>, value: errors, toolTip: "Antall feil i alle konsumere"},
+    {icon: <ArrowsCirclepathIcon aria-hidden/>, value: restarts, toolTip: "Antall restart av konsumere"},
+  ];
+
+  return <Tab
+    header={org}
+    fields={fields}
+    onClick={onClick}
+    className={className}
+  />;
 }
