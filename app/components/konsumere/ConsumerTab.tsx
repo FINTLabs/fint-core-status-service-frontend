bce5c8d33324202561326a140ea68ad6ce785bdf
Hknots <hknots99@gmail.com>
2025-03-14 10:19:26 +0100

    feat: create ConsumerTab

---
+++ app/components/konsumere/ConsumerTab.tsx
@@ -0,0 +1,31 @@
+import {ArrowsCirclepathIcon, XMarkOctagonIcon} from "@navikt/aksel-icons";
+import React from "react";
+import {Tab, TabField} from "~/components/konsumere/Tab";
+
+interface ConsumerTabProps {
+  consumer: string;
+  errors: number;
+  restarts: number;
+  onClick?: () => void;
+  className?: string;
+}
+
+export function ConsumerTab({
+                              consumer,
+                              errors,
+                              restarts,
+                              onClick,
+                              className,
+                            }: ConsumerTabProps) {
+  const fields: TabField[] = [
+    {icon: <XMarkOctagonIcon aria-hidden/>, value: errors, toolTip: "Antall feil i alle konsumere"},
+    {icon: <ArrowsCirclepathIcon aria-hidden/>, value: restarts, toolTip: "Antall restart av konsumere"},
+  ];
+
+  return <Tab
+    header={consumer}
+    fields={fields}
+    onClick={onClick}
+    className={className}
+  />;
+}
