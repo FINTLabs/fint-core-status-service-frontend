094d91a91a7531af563d431b2a98a3dfe721b9b1
sindre <sindre.giske@novari.no>
2025-07-29 14:15:22 +0200

    Updated function to not rely on deprecated json implementation

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,10 @@
 import {useNavigate} from "react-router";
-import {json, LoaderFunction} from "@remix-run/node";
+import {LoaderFunction} from "@remix-run/node";
 import {StatusApi} from "~/api/StatusApi";
 import {useLoaderData} from "@remix-run/react";
 import {IStats} from "~/types/IStats";
 import {formatStats} from "~/components/komponenter/Stats";
-import {Box, Heading} from "@navikt/ds-react";
+import {Box} from "@navikt/ds-react";
 import {envCookie} from "~/components/cookie";
 import {useEnv} from "~/constants/envContext";
 
@@ -19,23 +19,32 @@ let currentCookie: string;
 export const loader: LoaderFunction = async ({request}) => {
     const cookieHeader = request.headers.get("Cookie");
     currentCookie = await envCookie.parse(cookieHeader);
+
     try {
         const alpha: IStats = await StatusApi.getStats("alpha");
         const beta: IStats = await StatusApi.getStats("beta");
         const api: IStats = await StatusApi.getStats("api");
 
-        return json({
-            alpha: alpha,
-            beta: beta,
-            api: api,
-        });
+        return new Response(
+            JSON.stringify({alpha, beta, api}),
+            {
+                status: 200,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
     } catch (error) {
         console.error("Loader Error: ", error);
-        return json({
-            alpha: null,
-            beta: null,
-            api: null,
-        });
+        return new Response(
+            JSON.stringify({alpha: null, beta: null, api: null}),
+            {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
     }
 };
 
@@ -44,7 +53,7 @@ export default function Index() {
     const {setEnv} = useEnv();
     console.log(currentCookie);
     useNavigate();
-    
+
     return (
         <div className={"pt-12"}>
             <Box className="p-5" onClick={() => setEnv("api")}>
