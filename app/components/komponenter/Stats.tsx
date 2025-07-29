69fb7ee567d1520edfc1dd0cc64fee11ca7f004d
sindre <sindre.giske@novari.no>
2025-07-29 10:54:54 +0200

    Branch init, landing headers improved

--- app/components/komponenter/Stats.tsx
+++ app/components/komponenter/Stats.tsx
@@ -5,52 +5,53 @@ import {
     ArrowsSquarepathIcon,
     ExclamationmarkTriangleIcon,
     HeartIcon,
-    TasklistIcon
+    TasklistIcon,
+    SealCheckmarkFillIcon
 } from "@navikt/aksel-icons";
-import { Link } from '@remix-run/react';
+import {Link} from '@remix-run/react';
 
-
-export function formatStats(stats: IStats) {
+export function formatStats(stats: IStats, env: string) {
     return (
-        <HGrid gap="6" columns={{ xs: 1, sm: 1, md: 3 }}>
-            <Link to={"/kontrakter"}>
-            {/*<a href={"/kontrakter"} style={{textDecoration: "none", color: "inherit"}}>*/}
-                <Box padding={"10"} height={"200"} borderRadius='xlarge' background={"bg-subtle"} >
+        <Box>
+            <HStack className={"w-full border-b-2 mb-4"} align={"center"}>
+                <SealCheckmarkFillIcon title="a11y-title" fontSize="1.5rem"/>
+                <Heading size={"large"}>Miljø Oversikt - {env}</Heading>
+            </HStack>
+            <HGrid gap="6" columns={3}>
+                <Link to={"/kontrakter"}>
+                    <Box padding={"10"} height={"200"} borderRadius='xlarge' background={"bg-subtle"}>
+                        <Heading align="center" size="medium">
+                            Kontrakter
+                        </Heading>
+                        <HStack gap="1">
+                            <TasklistIcon title="Total Contracts" fontSize="1.5rem" style={{marginLeft: "20%"}}/>
+                            {stats.adapterContractAmount}
+                            <HeartIcon title="Healty heartbeats" fontSize="1.5rem" style={{marginLeft: "5%"}}/>
+                            {stats.hasContectAmount}
+                        </HStack>
+                    </Box></Link>
+                <Link to={"/hendelser"}>
+                    <Box padding={"10"} height={"200"} borderRadius='xlarge' background={"bg-subtle"}>
+                        <Heading align="center" size="medium">
+                            Hendelser
+                        </Heading>
+                        <HStack gap="1">
+                            <ArrowRightLeftIcon title="Total requests" fontSize="1.5rem" style={{marginLeft: "10%"}}/>
+                            {stats.eventAmount}
+                            <ArrowsSquarepathIcon title="Total responses" fontSize="1.5rem" style={{marginLeft: "5%"}}/>
+                            {stats.eventResponses}
+                            <ExclamationmarkTriangleIcon title="Errors" fontSize="1.5rem" style={{marginLeft: "5%"}}/>
+                            {stats.eventErrors}
+                        </HStack>
+                    </Box>
+                </Link>
+                <Box padding={"10"} height={"200"} borderRadius='xlarge' background={"bg-subtle"}>
                     <Heading align="center" size="medium">
-                        Kontrakter
+                        Konsumere
                     </Heading>
-                    <HStack gap="1">
-                        <TasklistIcon title="Total Contracts" fontSize="1.5rem" style={{marginLeft: "20%"}}/>
-                        {stats.adapterContractAmount}
-                        <HeartIcon title="Healty heartbeats" fontSize="1.5rem" style={{marginLeft: "5%"}}/>
-                        {stats.hasContectAmount}
-                    </HStack>
-                </Box></Link>
-            {/*</a>*/}
-            {/*<a href={"/hendelser"} style={{textDecoration: "none", color: "inherit"}}>*/}
-            <Link to={"/hendelser"}>
-                <Box padding={"10"} height={"200"} borderRadius='xlarge' background={"bg-subtle"} >
-                    <Heading align="center" size="medium">
-                        Hendelser
-                    </Heading>
-                    <HStack gap="1">
-                        <ArrowRightLeftIcon title="Total requests" fontSize="1.5rem" style={{marginLeft: "10%"}}/>
-                        {stats.eventAmount}
-                        <ArrowsSquarepathIcon title="Total responses" fontSize="1.5rem" style={{marginLeft: "5%"}}/>
-                        {stats.eventResponses}
-                        <ExclamationmarkTriangleIcon title="Errors" fontSize="1.5rem" style={{marginLeft: "5%"}}/>
-                        {stats.eventErrors}
-                    </HStack>
+                    <Detail style={{marginLeft: "30%"}}>Will be added later</Detail>
                 </Box>
-            {/*</a>*/}
-            </Link>
-            <Box padding={"10"} height={"200"} borderRadius='xlarge' background={"bg-subtle"} >
-                <Heading align="center" size="medium">
-                    Konsumere
-                </Heading>
-                <Detail style={{marginLeft: "30%"}}>Will be added later</Detail>
-            </Box>
-        </HGrid>
+            </HGrid>
+        </Box>
     )
-
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,65 +1,62 @@
-import { useNavigate } from "react-router";
-import { json, LoaderFunction } from "@remix-run/node";
-import { StatusApi } from "~/api/StatusApi";
-import { useLoaderData } from "@remix-run/react";
-import { IStats } from "~/types/IStats";
-import { formatStats } from "~/components/komponenter/Stats";
-import { Box, Heading } from "@navikt/ds-react";
-import { envCookie } from "~/components/cookie";
-import { useEnv } from "~/constants/envContext";
+import {useNavigate} from "react-router";
+import {json, LoaderFunction} from "@remix-run/node";
+import {StatusApi} from "~/api/StatusApi";
+import {useLoaderData} from "@remix-run/react";
+import {IStats} from "~/types/IStats";
+import {formatStats} from "~/components/komponenter/Stats";
+import {Box, Heading} from "@navikt/ds-react";
+import {envCookie} from "~/components/cookie";
+import {useEnv} from "~/constants/envContext";
 
 export interface IEnvStats {
-  api: IStats;
-  beta: IStats;
-  alpha: IStats;
+    api: IStats;
+    beta: IStats;
+    alpha: IStats;
 }
 
 let currentCookie: string;
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const cookieHeader = request.headers.get("Cookie");
-  currentCookie = await envCookie.parse(cookieHeader);
-  try {
-    const alpha: IStats = await StatusApi.getStats("alpha");
-    const beta: IStats = await StatusApi.getStats("beta");
-    const api: IStats = await StatusApi.getStats("api");
+export const loader: LoaderFunction = async ({request}) => {
+    const cookieHeader = request.headers.get("Cookie");
+    currentCookie = await envCookie.parse(cookieHeader);
+    try {
+        const alpha: IStats = await StatusApi.getStats("alpha");
+        const beta: IStats = await StatusApi.getStats("beta");
+        const api: IStats = await StatusApi.getStats("api");
 
-    return json({
-      alpha: alpha,
-      beta: beta,
-      api: api,
-    });
-  } catch (error) {
-    console.error("Loader Error: ", error);
-    return json({
-      alpha: null,
-      beta: null,
-      api: null,
-    });
-  }
+        return json({
+            alpha: alpha,
+            beta: beta,
+            api: api,
+        });
+    } catch (error) {
+        console.error("Loader Error: ", error);
+        return json({
+            alpha: null,
+            beta: null,
+            api: null,
+        });
+    }
 };
 
 export default function Index() {
-  const stats = useLoaderData<IEnvStats>();
-  const { setEnv } = useEnv();
-  console.log(currentCookie);
-  useNavigate();
+    const stats = useLoaderData<IEnvStats>();
+    const {setEnv} = useEnv();
+    console.log(currentCookie);
+    useNavigate();
 
-  //TODO: work in progrss, will be refactored
-  return (
-    <div>
-      <Box className="p-5" onClick={() => setEnv("api")}>
-        <Heading size={"medium"}>Api</Heading>
-        {stats.api ? formatStats(stats.api) : <p>No data available</p>}
-      </Box>
-      <Box className="p-3" onClick={() => setEnv("beta")}>
-        <Heading size={"medium"}>Beta</Heading>
-        {stats.beta ? formatStats(stats.beta) : <p>No data available</p>}
-      </Box>
-      <Box className="p-3" onClick={() => setEnv("alpha")}>
-        <Heading size={"medium"}>Alpha</Heading>
-        {stats.alpha ? formatStats(stats.alpha) : <p>No data available</p>}
-      </Box>
-    </div>
-  );
+    //TODO: work in progrss, will be refactored
+    return (
+        <div className={"pt-12"}>
+            <Box className="p-5" onClick={() => setEnv("api")}>
+                {stats.api ? formatStats(stats.api, "Api") : <p>No data available</p>}
+            </Box>
+            <Box className="p-3" onClick={() => setEnv("beta")}>
+                {stats.beta ? formatStats(stats.beta, "Beta") : <p>No data available</p>}
+            </Box>
+            <Box className="p-3" onClick={() => setEnv("alpha")}>
+                {stats.alpha ? formatStats(stats.alpha, "Alpha") : <p>No data available</p>}
+            </Box>
+        </div>
+    );
 }
