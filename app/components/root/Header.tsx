9b23a4f632ca76e2edc76e50aa9ee88266f3cbc5
sindre <sindre.giske@novari.no>
2025-08-13 15:12:00 +0200

    Novari-Frontend-Components updated to 1.12.1, header adjusted accordingly.

--- app/components/root/Header.tsx
+++ app/components/root/Header.tsx
@@ -3,6 +3,13 @@ import {EnvSelector} from "~/constants/envSelector";
 import {NovariHeader} from "novari-frontend-components";
 import {useNavigate} from "react-router";
 import {Heading} from "@navikt/ds-react";
+import {
+    BriefcaseIcon,
+    PadlockLockedIcon,
+    PersonCircleIcon,
+    PersonGroupIcon,
+    QuestionmarkCircleIcon
+} from "@navikt/aksel-icons";
 
 interface headerProps {
     onHeaderChange: (newEnv: string) => void;
@@ -14,22 +21,48 @@ export default function Header({onHeaderChange, value}: headerProps) {
 
     return (
         <>
-            <NovariHeader appName={"Kundeportalen"} showLogoWithTitle={true} menu={[
+            <NovariHeader appName={"Kundeportalen"} showLogoWithTitle={true}
+                          menu={[
                 ["Home", "/"],
                 {
                     label: "Main Sections",
                     items: [
-                        ["About", "/about"],
-                        ["Team", "/team"],
-                        ["Careers", "/careers"],
+                        {
+                            label: "About",
+                            action: "/about",
+                            icon: <QuestionmarkCircleIcon title="a11y-title" fontSize="1.5rem"/>,
+                            disabled: true
+                        },
+                        {
+                            label: "Team",
+                            action: "/team",
+                            icon: <PersonGroupIcon title="a11y-title" fontSize="1.5rem"/>,
+                            disabled: true
+                        },
+                        {
+                            label: "Careers",
+                            action: "/careers",
+                            icon: <BriefcaseIcon title="a11y-title" fontSize="1.5rem"/>,
+                            disabled: true
+                        },
                     ],
                 },
                 ["Contact", "/contact"],
                 {
                     label: "Settings",
                     items: [
-                        ["Profile", "/profile"],
-                        ["Security", "/security"],
+                        {
+                            label: "Profile",
+                            action: "/profile",
+                            icon: <PersonCircleIcon title="a11y-title" fontSize="1.5rem"/>,
+                            disabled: true
+                        },
+                        {
+                            label: "Security",
+                            action: "/security",
+                            icon: <PadlockLockedIcon title="a11y-title" fontSize="1.5rem"/>,
+                            disabled: true
+                        },
                     ],
                 },
             ]}
